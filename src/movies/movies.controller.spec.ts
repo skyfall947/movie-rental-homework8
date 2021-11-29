e885bb385288ac6be1addfcfa58c72cb6b4c147d
skyfall947 <atencio947@gmail.com>
2021-11-29 18:05:41 -0400

    Add unit test for filter and sort functionality

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -1,36 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MovieDto } from './dto/movie.dto';
+import { MovieQueries } from './dto/query-movie.dto';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
 describe('MoviesController', () => {
-  let controller: MoviesController;
+  let moviesController: MoviesController;
+  let moviesService: MoviesService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [MoviesController],
       providers: [
         {
           provide: MoviesService,
           useValue: {
-            insertOne: jest.fn(),
-            findOne: jest.fn(),
-            getAll: jest.fn(),
-            updateOne: jest.fn(),
-            removeOne: jest.fn(),
-            removeAll: jest.fn(),
-            rentMovie: jest.fn(),
-            unRentMovie: jest.fn(),
-            saleMovie: jest.fn(),
-            tagMovie: jest.fn(),
+            findAll: jest.fn().mockResolvedValue([new MovieDto()]),
+            sortByTitle: jest.fn().mockResolvedValue([new MovieDto()]),
+            sortByLikes: jest.fn().mockResolvedValue([new MovieDto()]),
+            filterByTags: jest.fn().mockResolvedValue([new MovieDto()]),
           },
         },
       ],
     }).compile();
 
-    controller = module.get<MoviesController>(MoviesController);
+    moviesController = moduleRef.get<MoviesController>(MoviesController);
+    moviesService = moduleRef.get<MoviesService>(MoviesService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(moviesController).toBeDefined();
+  });
+
+  describe('findAll()', () => {
+    it('should retrieve a list of movies', () => {
+      const queries: MovieQueries = {
+        sort: 'title,likes',
+        title: 'avengers',
+        available: true,
+        tags: 'comedy,action',
+      };
+      expect(moviesController.findAll(queries)).resolves.toHaveLength(1);
+      expect(moviesService.findAll).toBeCalledWith(
+        queries.available,
+        queries.title,
+      );
+    });
   });
 });
--- test/mocks/movies-mock.ts
+++ test/mocks/movies-mock.ts
@@ -8,3 +8,68 @@ export const movieToCreate: CreateMovieDto = {
   likes: 1,
   description: 'Description movie',
 };
+
+export const unsortedMoviesTitle = [
+  {
+    title: 'Bbb',
+  },
+  {
+    title: 'Ccc',
+  },
+  {
+    title: 'Aaa',
+  },
+];
+export const sortedMoviesTitleAsc = [
+  {
+    title: 'Aaa',
+  },
+  {
+    title: 'Bbb',
+  },
+  {
+    title: 'Ccc',
+  },
+];
+
+export const unsortedMoviesLikes = [
+  {
+    likes: 15,
+  },
+  {
+    likes: 20,
+  },
+  {
+    likes: 10,
+  },
+];
+export const sortedMoviesLikesAsc = [
+  {
+    likes: 10,
+  },
+  {
+    likes: 15,
+  },
+  {
+    likes: 20,
+  },
+];
+
+export const moviesToSortByTitle = [
+  {
+    title: 'Avengers',
+    tags: [{ title: 'comedy' }, { title: 'action' }],
+  },
+  {
+    title: 'Coco',
+    tags: [{ title: 'comedy' }, { title: 'animation' }],
+  },
+  {
+    title: 'Dune',
+    tags: [{ title: 'sci-fi' }, { title: 'action' }],
+  },
+  {
+    title: 'Hamilton',
+    tags: [{ title: 'musical' }, { title: 'documetary' }],
+  },
+];
